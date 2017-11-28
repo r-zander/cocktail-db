2c1fb0fa2334120219acc1a1e2055b2289ae4e96
Raoul Zander <raoulzander@oviva.com>
2017-11-28 19:46:12 +0100

    Wired up backend to create cocktails with recipe.

--- frontend/src/api/database.service.js
+++ frontend/src/api/database.service.js
@@ -1,35 +1,85 @@
 export class DatabaseService {
-	constructor($http) {
-		'ngInject';
+    constructor($http) {
+        'ngInject';
 
-		this.baseUrl = '//localhost/cocktail-db/api.php/';
-		this.$http = $http;
-	}
+        this.baseUrl = '//localhost/cocktail-db/api.php/';
+        this.$http = $http;
+    }
 
-	getCocktails() {
-		return this.$http({
-			method: 'GET',
-			url: this.baseUrl + 'cocktail' + '?transform=1',
-		}).then(response => {
-			return response.data.cocktail;
-		});
-	}
+    getCocktails() {
+        return this.$http({
+            method: 'GET',
+            url: this.baseUrl + 'cocktail' + '?transform=1',
+        }).then(response => {
+            return response.data.cocktail;
+        });
+    }
 
-	getInventory() {
-		return this.$http({
-			method: 'GET',
-			url: this.baseUrl + 'zutat' + '?transform=1',
-		}).then(response => {
-			return response.data.zutat;
-		});
-	}
+    getInventory() {
+        return this.$http({
+            method: 'GET',
+            url: this.baseUrl + 'zutat' + '?transform=1',
+        }).then(response => {
+            return response.data.zutat;
+        });
+    }
 
-	getMixableDrinks() {
-		return this.$http({
-			method: 'GET',
-			url: this.baseUrl + 'machbare_cocktails' + '?transform=1',
-		}).then(response => {
-			return response.data.machbare_cocktails;
-		});
-	}
+    getMixableDrinks() {
+        return this.$http({
+            method: 'GET',
+            url: this.baseUrl + 'machbare_cocktails' + '?transform=1',
+        }).then(response => {
+            return response.data.machbare_cocktails;
+        });
+    }
+
+    /**
+     * Creates a new cocktail in the database and links t
+     * he provided ingredients as recipe.
+     *
+     * @param recipe
+     * @return Promise<number> id of new cocktail
+     */
+    createCocktailWithRecipe(recipe) {
+        return this.$http({
+            method: 'POST',
+            url: this.baseUrl + 'cocktail',
+            data: {
+                Name: recipe.name,
+            },
+        }).then(response => {
+            return response.data;
+        }).then(newCocktailId => {
+            let recipeRecords = [];
+            recipe.ingredients.forEach(ingredient => {
+                recipeRecords.push({
+                    Cocktail_ID: newCocktailId,
+                    Zutat_ID: ingredient.id,
+                    Menge: ingredient.amount,
+                });
+            });
+
+            return this.$http({
+                method: 'POST',
+                url: this.baseUrl + 'rezept',
+                data: recipeRecords,
+            }).then(() => {
+                return newCocktailId;
+            });
+        });
+    }
+
+    createIngredient(ingredient) {
+        return this.$http({
+            method: 'POST',
+            url: this.baseUrl + 'zutat',
+            data: {
+                Name: ingredient.name,
+                Menge: ingredient.amount,
+                Einheit: ingredient.unit,
+            },
+        }).then(response => {
+            return response.data;
+        });
+    }
 }
--- frontend/src/main/main.controller.js
+++ frontend/src/main/main.controller.js
@@ -5,127 +5,149 @@ import './bottomSheet.html';
 /* global angular */
 
 export class MainController {
-	constructor($mdBottomSheet, databaseService) {
-		'ngInject';
+    constructor($mdBottomSheet, databaseService) {
+        'ngInject';
 
-		this.$mdBottomSheet = $mdBottomSheet;
+        this.$mdBottomSheet = $mdBottomSheet;
+        this.databaseService = databaseService;
 
-		// TODO alles in der Datenbank sortieren
-		databaseService.getCocktails().then(cocktails => {
-			this.cocktails = cocktails
-		});
-		databaseService.getInventory().then(inventory => {
-			this.inventory = inventory.filter(item => {
-				return item.Inventarmenge > 0;
-			});
-			this.ingredients = inventory;
-		});
-		databaseService.getMixableDrinks().then(mixableDrinks => {
-			this.mixableDrinks = mixableDrinks.filter(drink => {
-				// TODO muss in der Datenbank passieren
-				return drink.Anzahl > 0;
-			});
-		});
+        this.loadLists();
 
-		this.selectedTab = 1;
-	}
+        this.selectedTab = 1;
+    }
 
-	openBottomSheet() {
-		this.$mdBottomSheet.show({
-			templateUrl: 'main/bottomSheet.html',
-			preserveScope: true,
-			controller: BottomSheetController,
-			controllerAs: 'ctrl',
-			locals: {
-				selectedTab: () => {
-					return this.selectedTab;
-				},
-				ingredients: this.ingredients
-			},
-			bindToController: true,
+    loadLists() {
+// TODO alles in der Datenbank sortieren
+        this.databaseService.getCocktails().then(cocktails => {
+            this.cocktails = cocktails
+        });
+        this.databaseService.getInventory().then(inventory => {
+            this.inventory = inventory.filter(item => {
+                return item.Inventarmenge > 0;
+            });
+            this.ingredients = inventory;
+        });
+        this.databaseService.getMixableDrinks().then(mixableDrinks => {
+            this.mixableDrinks = mixableDrinks.filter(drink => {
+                // TODO muss in der Datenbank passieren
+                return drink.Anzahl > 0;
+            });
+        });
+    }
+
+    openBottomSheet() {
+        this.$mdBottomSheet.show({
+            templateUrl: 'main/bottomSheet.html',
+            preserveScope: true,
+            controller: BottomSheetController,
+            controllerAs: 'ctrl',
+            locals: {
+                selectedTab: () => {
+                    return this.selectedTab;
+                },
+                ingredients: this.ingredients,
+            },
+            bindToController: true,
             escapeToClose: false,
-		}).then(result => {
-			console.log(result);
-		}).catch(() => {});
-	}
+        }).then(result => {
+            return this.databaseService.createCocktailWithRecipe(result.cocktail);
+        }).then(() => {
+            this.loadLists();
+        }).catch(() => {
+        });
+    }
 }
 
 class BottomSheetController {
-	constructor($scope, $mdBottomSheet) {
-		'ngInject';
+    constructor($scope, $mdBottomSheet, databaseService) {
+        'ngInject';
 
-		this.$scope = $scope;
-		this.$mdBottomSheet = $mdBottomSheet;
+        this.$scope = $scope;
+        this.$mdBottomSheet = $mdBottomSheet;
+        this.databaseService = databaseService;
 
-		$scope.cocktail = {
-			ingredients: []
-		};
+        $scope.cocktail = {
+            ingredients: [],
+        };
 
-		this.ensureIngredientsCapacity(1);
-	}
+        this.ensureIngredientsCapacity(1);
+    }
 
-	ensureIngredientsCapacity(capacity) {
-		let ingredients = this.$scope.cocktail.ingredients;
+    ensureIngredientsCapacity(capacity) {
+        let ingredients = this.$scope.cocktail.ingredients;
 
-		if (capacity > ingredients.length) {
-			for (let i = ingredients.length; i < capacity; i++) {
-				this.$scope.cocktail.ingredients.push({});
-			}
-		} else {
-			ingredients.length = capacity - 1;
-		}
-	}
+        if (capacity > ingredients.length) {
+            for (let i = ingredients.length; i < capacity; i++) {
+                this.$scope.cocktail.ingredients.push({});
+            }
+        } else {
+            ingredients.length = capacity - 1;
+        }
+    }
 
-	querySearch(query) {
-		if (query) {
-			return this.ingredients.filter((ingredient) => {
-				let lowercaseQuery = angular.lowercase(query);
-				return (ingredient.Name.indexOf(lowercaseQuery) === 0)
-			});
-		}
+    querySearch(query) {
+        if (query) {
+            return this.ingredients.filter((ingredient) => {
+                let lowercaseQuery = query.toLowerCase();
+                return (ingredient.Name.indexOf(lowercaseQuery) === 0)
+            });
+        }
 
-		return this.ingredients;
-	}
+        return this.ingredients;
+    }
 
-	isValid(){
+    newIngredient(ingredient, newIngredientName) {
+        this.databaseService.createIngredient({name: newIngredientName})
+            .then(newIngredientId => {
+                ingredient.item = {
+                    ID: newIngredientId,
+                    Name: newIngredientName,
+                    new: true,
+                };
+
+                this.ingredients.push(ingredient.item);
+            });
+    }
+
+    isValid() {
         let cocktail = this.$scope.cocktail;
-        if (!cocktail.name){
-			return false;
-		}
+        if (!cocktail.name) {
+            return false;
+        }
 
-		if (cocktail.ingredients.length < 2){
-        	return false;
-		}
+        if (cocktail.ingredients.length < 2) {
+            return false;
+        }
 
-		// At least 2 valid ingredients are required
-		return cocktail.ingredients.filter((ingredient) => {
-			return ingredient.item && ingredient.item.Name && ingredient.amount > 0;
-		}).length >= 2;
-	}
+        // At least 2 valid ingredients are required
+        return cocktail.ingredients.filter((ingredient) => {
+            return ingredient.item && ingredient.item.Name && ingredient.amount > 0;
+        }).length >= 2;
+    }
 
-    cancel(){
-		this.$mdBottomSheet.cancel();
-	}
+    cancel() {
+        this.$mdBottomSheet.cancel();
+    }
 
-	save(){
+    save() {
         let cocktail = this.$scope.cocktail;
 
         let result = {
-    		cocktail: {
-    			name: cocktail.name,
-				ingredients: [],
-			}
-		};
+            cocktail: {
+                name: cocktail.name,
+                ingredients: [],
+            },
+        };
 
         cocktail.ingredients.forEach(ingredient => {
-        	if (angular.isObject(ingredient.item) &&
+            if (angular.isObject(ingredient.item) &&
                 angular.isNumber(ingredient.amount)) {
                 result.cocktail.ingredients.push({
                     id: ingredient.item.ID,
-                    amount: ingredient.amount
+                    amount: ingredient.amount,
                 });
             }
-		});
+        });
 
         this.$mdBottomSheet.hide(result);
     }
