5ff97667bd93a4a7dc22cc1b568e7d22bac91047
xyckno <Xyckno@gmail.com>
2017-12-02 15:33:03 +0100

    all entitynames edited, changed to eng, reedit view

--- backend/import.sql
+++ backend/import.sql
@@ -1,10 +1,12 @@
+DROP Database `cocktail_db`;
+
 CREATE DATABASE  IF NOT EXISTS `cocktail_db` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `cocktail_db`;
--- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
 --
--- Host: et-ops.ovivacoach.com    Database: cocktail_db
+-- Host: 127.0.0.1    Database: cocktail_db
 -- ------------------------------------------------------
--- Server version	5.5.57-MariaDB-1~wheezy
+-- Server version	5.7.20-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -25,9 +27,9 @@ DROP TABLE IF EXISTS `cocktail`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `cocktail` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `Name` varchar(255) NOT NULL,
-  PRIMARY KEY (`ID`)
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -42,102 +44,94 @@ INSERT INTO `cocktail` VALUES (1,'Gin Tonic'),(2,'Negroni'),(3,'VAC');
 UNLOCK TABLES;
 
 --
--- Temporary view structure for view `cocktail liste`
+-- Temporary view structure for view `cocktail_list`
 --
 
-DROP TABLE IF EXISTS `cocktail liste`;
-/*!50001 DROP VIEW IF EXISTS `cocktail liste`*/;
+DROP TABLE IF EXISTS `cocktail_list`;
+/*!50001 DROP VIEW IF EXISTS `cocktail_list`*/;
 SET @saved_cs_client     = @@character_set_client;
 SET character_set_client = utf8;
-/*!50001 CREATE VIEW `cocktail liste` AS SELECT 
- 1 AS `Cocktail`,
- 1 AS `Zutat`,
- 1 AS `Menge`,
- 1 AS `Inventarmenge`,
- 1 AS `Einheit`*/;
+/*!50001 CREATE VIEW `cocktail_list` AS SELECT 
+ 1 AS `cocktail`,
+ 1 AS `ingredient`,
+ 1 AS `amount`,
+ 1 AS `stock_amount`,
+ 1 AS `unit`*/;
 SET character_set_client = @saved_cs_client;
 
 --
--- Temporary view structure for view `machbare_cocktails`
+-- Table structure for table `ingredient`
 --
 
-DROP TABLE IF EXISTS `machbare_cocktails`;
-/*!50001 DROP VIEW IF EXISTS `machbare_cocktails`*/;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
-/*!50001 CREATE VIEW `machbare_cocktails` AS SELECT 
- 1 AS `Cocktail`,
- 1 AS `Anzahl`*/;
-SET character_set_client = @saved_cs_client;
-
---
--- Table structure for table `rezept`
---
-
-DROP TABLE IF EXISTS `rezept`;
+DROP TABLE IF EXISTS `ingredient`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `rezept` (
-  `Cocktail_ID` int(11) NOT NULL,
-  `Zutat_ID` int(11) NOT NULL,
-  `Menge` smallint(6) NOT NULL,
-  PRIMARY KEY (`Cocktail_ID`,`Zutat_ID`),
-  KEY `fk_Rezepte_Cocktail1_idx` (`Cocktail_ID`),
-  KEY `fk_Rezepte_Zutat1_idx` (`Zutat_ID`),
-  CONSTRAINT `fk_Rezepte_Cocktail1` FOREIGN KEY (`Cocktail_ID`) REFERENCES `cocktail` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `fk_Rezepte_Zutat1` FOREIGN KEY (`Zutat_ID`) REFERENCES `zutat` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+CREATE TABLE `ingredient` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `stock_amount` smallint(6) NOT NULL,
+  `unit` varchar(10) NOT NULL COMMENT 'zB.: ml, cl, Scheiben oder Stück',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ingredient`
+--
+
+LOCK TABLES `ingredient` WRITE;
+/*!40000 ALTER TABLE `ingredient` DISABLE KEYS */;
+INSERT INTO `ingredient` VALUES (1,'Gin',400,'ml'),(2,'Tonic Water',1300,'ml'),(3,'Zitrone',10,'Scheibe'),(4,'Campari',0,'ml'),(5,'Roter Wermut',0,'ml'),(6,'Pineapple Juice',100,'ml'),(7,'Coca Cola',1000,'ml'),(8,'Skyy Vodka',700,'ml');
+/*!40000 ALTER TABLE `ingredient` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Temporary view structure for view `mixable_drinks`
+--
+
+DROP TABLE IF EXISTS `mixable_drinks`;
+/*!50001 DROP VIEW IF EXISTS `mixable_drinks`*/;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+/*!50001 CREATE VIEW `mixable_drinks` AS SELECT 
+ 1 AS `cocktail`,
+ 1 AS `count`*/;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Table structure for table `recipe`
+--
+
+DROP TABLE IF EXISTS `recipe`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `recipe` (
+  `cocktail_id` int(11) NOT NULL,
+  `ingredient_id` int(11) NOT NULL,
+  `amount` smallint(6) NOT NULL,
+  PRIMARY KEY (`cocktail_id`,`ingredient_id`),
+  KEY `fk_Rezepte_Cocktail1_idx` (`cocktail_id`),
+  KEY `fk_Rezepte_Zutat1_idx` (`ingredient_id`),
+  CONSTRAINT `fk_Rezepte_Cocktail1` FOREIGN KEY (`cocktail_id`) REFERENCES `cocktail` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_Rezepte_Zutat1` FOREIGN KEY (`ingredient_id`) REFERENCES `ingredient` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `rezept`
+-- Dumping data for table `recipe`
 --
 
-LOCK TABLES `rezept` WRITE;
-/*!40000 ALTER TABLE `rezept` DISABLE KEYS */;
-INSERT INTO `rezept` VALUES (1,1,40),(1,2,460),(1,3,1),(2,1,30),(2,4,30),(2,5,30);
-/*!40000 ALTER TABLE `rezept` ENABLE KEYS */;
+LOCK TABLES `recipe` WRITE;
+/*!40000 ALTER TABLE `recipe` DISABLE KEYS */;
+INSERT INTO `recipe` VALUES (1,1,40),(1,2,460),(1,3,1),(2,1,30),(2,4,30),(2,5,30),(3,6,210),(3,7,40),(3,8,50);
+/*!40000 ALTER TABLE `recipe` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `zutat`
+-- Final view structure for view `cocktail_list`
 --
 
-DROP TABLE IF EXISTS `zutat`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `zutat` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `Name` varchar(255) NOT NULL,
-  `Inventarmenge` smallint(6) NOT NULL,
-  `Einheit` varchar(10) NOT NULL COMMENT 'zB.: ml, cl, Scheiben oder Stück',
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `zutat`
---
-
-LOCK TABLES `zutat` WRITE;
-/*!40000 ALTER TABLE `zutat` DISABLE KEYS */;
-INSERT INTO `zutat` VALUES (1,'Gin',400,'ml'),(2,'Tonic Water',1300,'ml'),(3,'Zitrone',10,'Scheibe'),(4,'Campari',0,'ml'),(5,'Roter Wermut',0,'ml');
-/*!40000 ALTER TABLE `zutat` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Dumping events for database 'cocktail_db'
---
-
---
--- Dumping routines for database 'cocktail_db'
---
-
---
--- Final view structure for view `cocktail liste`
---
-
-/*!50001 DROP VIEW IF EXISTS `cocktail liste`*/;
+/*!50001 DROP VIEW IF EXISTS `cocktail_list`*/;
 /*!50001 SET @saved_cs_client          = @@character_set_client */;
 /*!50001 SET @saved_cs_results         = @@character_set_results */;
 /*!50001 SET @saved_col_connection     = @@collation_connection */;
@@ -146,16 +140,16 @@ UNLOCK TABLES;
 /*!50001 SET collation_connection      = utf8_general_ci */;
 /*!50001 CREATE ALGORITHM=UNDEFINED */
 /*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
-/*!50001 VIEW `cocktail liste` AS select `c`.`Name` AS `Cocktail`,`z`.`Name` AS `Zutat`,`r`.`Menge` AS `Menge`,`z`.`Inventarmenge` AS `Inventarmenge`,`z`.`Einheit` AS `Einheit` from ((`rezept` `r` join `cocktail` `c` on((`r`.`Cocktail_ID` = `c`.`ID`))) join `zutat` `z` on((`r`.`Zutat_ID` = `z`.`ID`))) order by `c`.`Name` */;
+/*!50001 VIEW `cocktail_list` AS select `c`.`name` AS `cocktail`,`i`.`name` AS `ingredient`,`r`.`amount` AS `amount`,`i`.`stock_amount` AS `stock_amount`,`i`.`unit` AS `unit` from ((`recipe` `r` join `cocktail` `c` on((`r`.`cocktail_id` = `c`.`id`))) join `ingredient` `i` on((`r`.`ingredient_id` = `i`.`id`))) order by `c`.`name` */;
 /*!50001 SET character_set_client      = @saved_cs_client */;
 /*!50001 SET character_set_results     = @saved_cs_results */;
 /*!50001 SET collation_connection      = @saved_col_connection */;
 
 --
--- Final view structure for view `machbare_cocktails`
+-- Final view structure for view `mixable_drinks`
 --
 
-/*!50001 DROP VIEW IF EXISTS `machbare_cocktails`*/;
+/*!50001 DROP VIEW IF EXISTS `mixable_drinks`*/;
 /*!50001 SET @saved_cs_client          = @@character_set_client */;
 /*!50001 SET @saved_cs_results         = @@character_set_results */;
 /*!50001 SET @saved_col_connection     = @@collation_connection */;
@@ -164,7 +158,7 @@ UNLOCK TABLES;
 /*!50001 SET collation_connection      = utf8_general_ci */;
 /*!50001 CREATE ALGORITHM=UNDEFINED */
 /*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
-/*!50001 VIEW `machbare_cocktails` AS select `c`.`Name` AS `Cocktail`,floor(min((`z`.`Inventarmenge` / `r`.`Menge`))) AS `Anzahl` from ((`rezept` `r` join `cocktail` `c` on((`c`.`ID` = `r`.`Cocktail_ID`))) join `zutat` `z` on((`z`.`ID` = `r`.`Zutat_ID`))) group by `c`.`ID` */;
+/*!50001 VIEW `mixable_drinks` AS select `c`.`name` AS `cocktail`,floor(min((`i`.`stock_amount` / `r`.`amount`))) AS `count` from ((`recipe` `r` join `cocktail` `c` on((`c`.`id` = `r`.`cocktail_id`))) join `ingredient` `i` on((`i`.`id` = `r`.`ingredient_id`))) group by `c`.`id` */;
 /*!50001 SET character_set_client      = @saved_cs_client */;
 /*!50001 SET character_set_results     = @saved_cs_results */;
 /*!50001 SET collation_connection      = @saved_col_connection */;
@@ -178,4 +172,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-11-30 19:54:53
+-- Dump completed on 2017-12-02 15:29:37
